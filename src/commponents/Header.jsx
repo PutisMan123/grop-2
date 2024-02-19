746d674043f772bc6d9ad26ee343ad8ae6111346
KirKosh <101568071+KirKosh@users.noreply.github.com>
2024-02-19 06:47:46 +0000

    sdf

--- src/commponents/Header.jsx
+++ src/commponents/Header.jsx
@@ -4,45 +4,47 @@ import sales from "./../assets/sales.png";
 import cart from "./../assets/cart.png";
 import contacts from "./../assets/contacts.png";
 import Search from "./Search";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="flex justify-around items-center relative">
+    <header className="flex justify-around items-center fixed">
       <div className="bg-neutral-300 opacity-50 rounded-b-full absolute size-full"></div>
       <div className="flex justify-around items-center relative z-10 w-full">
         <img src={logo} />
         <nav className="header-nav">
           <ul className="flex xl:gap-11 lg:gap-4">
             <li>
-              <a href="" className="flex gap-1">
+              <Link to="./catalog" className="flex gap-1">
                 <img src={catalog} />
                 Каталог
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="" className="flex gap-1">
-                <img src={sales} />
+              <Link to="sales" className="flex gap-1">
+                <img src={sales} />{" "}
+                {/* Чтобы сделать якорь прописать <div id="sales"></div> */}
                 Скидки
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="" className="flex gap-1">
+              <Link to="cart" className="flex gap-1">
                 <img src={cart} />
                 Корзина
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="" className="flex gap-1">
-                <img src={contacts} />
+              <Link to="footer" className="flex gap-1">
+                <img src={contacts} /> {/* Якорь на подвал*/}
                 Контакты
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
         <Search />
-        <a href="#" className="header__profile-link">
+        <Link to="#" className="header__profile-link">
           Личный кабинет
-        </a>
+        </Link>
       </div>
     </header>
   );
